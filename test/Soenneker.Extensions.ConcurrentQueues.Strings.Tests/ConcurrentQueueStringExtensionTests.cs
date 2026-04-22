2bc76fd41431fee1bdfd93ba65e8c78b582ff724
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:50 -0500

    TUnit conversion

--- test/Soenneker.Extensions.ConcurrentQueues.Strings.Tests/ConcurrentQueueStringExtensionTests.cs
+++ test/Soenneker.Extensions.ConcurrentQueues.Strings.Tests/ConcurrentQueueStringExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.ConcurrentQueues.Strings.Tests;
 
 public sealed class ConcurrentQueueStringExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
